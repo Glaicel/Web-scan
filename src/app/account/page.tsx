046209c4c991e6aba6d476aeeff7c255379bcf5d
xdeelord24 <jundeemark.molina@carsu.edu.ph>
2024-07-31 10:06:29 +0800

    First commit

---
+++ src/app/account/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../pages/components/Navbar'
+
+export default function Account() {
+  return (
+    <div>
+      <Navbar/>
+  <div className='flex items-center justify-center min-h-screen bg-gray-100 p-4'>
+      <h1 className='text-3xl font-bold'>Account Page</h1>
+    </div>
+    </div>
+   
+  )
+}
